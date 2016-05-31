72e3f5e517a219069c7bf0c61f1c7775d125119b
Charlie Thiry <charles.thiry@pokitdok.com>
2016-05-31 10:36:26 -0400

    ENTERPRISE-281: create setup.sh, serve.sh

---
+++ setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#install local docker deps
+brew install Caskroom/cask/virtualbox docker docker-machine docker-compose
+
+# create the pokitdok vm
+docker-machine create apidocs --driver virtualbox --virtualbox-memory 4048
+
+eval $(docker-machine env apidocs)
+docker create ruby:2.3
